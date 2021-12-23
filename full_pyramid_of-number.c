09d6757215333c42bffa1fa86bf3aa764a4da181
HEMANT15112003 <93367586+HEMANT15112003@users.noreply.github.com>
2021-12-23 14:59:00 +0530

    Add files via upload

---
+++ full_pyramid_of-number.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+int main()
+{
+	int i,j,k,s,n;
+	printf(" rows:");
+	scanf("%d", &n);
+	for(i=1;i<=n;i++)
+	{
+		for(s=1;s<=n-i;s++)
+		{
+			printf(" ");
+		}
+	
+	k=i;
+	for(j=1;j<=i;j++)
+	{
+		printf("%d", k++);
+	}
+	k = k-2;
+	for(j=1;j<i;j++)
+	{
+	printf("%d", k--);	
+	}
+	printf("\n");
+}
+return 0;
+}
