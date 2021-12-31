4313ed4de159037c1e2bee08045bbca652914e34
HEMANT15112003 <93367586+HEMANT15112003@users.noreply.github.com>
2021-12-31 21:43:27 +0530

    Add files via upload

---
+++ module-2/alphabet-half_pyramid.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main()
+{
+	int rows,i,j;
+	char ch = 'A';
+	printf("enter the number rows : ");
+	scanf("%d",&rows);
+	
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%c",'A'+j-1);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
